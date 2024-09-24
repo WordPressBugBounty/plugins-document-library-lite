b0858f7ce371ee615873bd06cf31c3047bb0bfa8
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 12:04:26 +0000

    1.0.7

---
+++ document-library-lite/dependencies/barn2/setup-wizard/build/setup-wizard.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Barn2\Plugin\Document_Library\Dependencies;
+
+return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0006010b20546586e246');
---
+++ document-library-lite/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd8439be35538415544fc0748029e545783727b56',
+        'name' => 'barn2/document-library-lite',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'barn2/document-library-lite' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd8439be35538415544fc0748029e545783727b56',
+            'dev_requirement' => false,
+        ),
+    ),
+);
